933921329251f26a1e91584b8a5b65acaa4fcd1d
Felipe Gustavo <55498940+felipe-gustavo@users.noreply.github.com>
2021-03-01 13:49:03 -0300

    Add basic control state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [buttonClicks, setButtonClicks] = useState(null);
+
+  const handleButtonClick = () => {
+    setButtonClicks((buttonClicks ?? 0) + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Controle de estado simples:
+          <div>
+            <button onClick={handleButtonClick}>{!buttonClicks ? 'Começar contagem' : `Clicou ${buttonClicks}`}</button>
+          </div>
         </p>
         <a
           className="App-link"
